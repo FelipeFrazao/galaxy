a36ec85e8a97b3687b1d053c928488679ecfa18c
Felipe Frazão <felipe.sfrazao@outlook.com>
2018-12-09 20:25:58 -0200

    SwApiService included

--- tests/unit/services/test_swapi.py
+++ tests/unit/services/test_swapi.py
@@ -1,20 +1,27 @@
 import os
 
 from flask_testing import TestCase
+from unittest import mock
+# from unittest.mock import patch
 
 from app import app
 from galaxy.services.SwApiService import SwApiService
 
 
-class TestIntegrationSWApi(TestCase):
-    def __init__(self):
-        self.swapi_host = os.environ.get("SWAPI_HOST")
+class TestUnitSwApiService(TestCase):
+    """
+        Unit test for Service SwApi
+    """
 
     def create_app(self):
-        app.config["TESTING"] = True
+        app.config['TESTING'] = True
         return app
 
-    def test_get_planets_info(self):
-        apparitions, population = SwApiService().get_planets_info("Tatooine")
+    @mock.patch.dict(os.environ, {"SWAPI_HOST": "https://swapi.co/api/"})
+    def test_get_planet_info(self):
+        apparitions, population = SwApiService().get_planet_info("Tatooine")
         self.assertEqual(apparitions, 5)
         self.assertEqual(population, 200000)
+
+    # def test_execute_request(self):
+    #     SwApiService.execute_request("planets/?=Tatooine")
