84d2d5c7349f88e11da82a8923329a4c685f4387
Felipe Frazão <felipe.sfrazao@outlook.com>
2018-12-13 09:13:42 -0200

    test planet routes

--- tests/unit/rest/test_planet_routes.py
+++ tests/unit/rest/test_planet_routes.py
@@ -16,7 +16,40 @@ class TestUnitPlanetRoutes(TestCase):
         app.config['TESTING'] = True
         return app
 
+    def setUp(self):
+        self.list_planet = [
+            {
+                "_id": "80884966-d276-4066-9f6c-718ce2ffc11e",
+                "name": "Yavin IV",
+                "climate": ["temperate", "tropical"],
+                "terrain": ["jungle", "rainforests"],
+                "apparitions": 4,
+                "population": 20000,
+            },
+            {
+                "_id": "5dda99a9-335a-4768-8ed7-0acb9f59944a",
+                "name": "Tatooine",
+                "climate": ["arid"],
+                "terrain": ["desert"],
+                "apparitions": 2,
+                "population": 23000,
+            },
+            {
+                "_id": "cf0aba97-da17-4e8c-bc58-8b5418cb7efa",
+                "name": "Alderaan",
+                "climate": ["temperate"],
+                "terrain": ["grasslands", "mountains"],
+                "apparitions": 3,
+                "population": 200,
+            },
+        ]
+
     @patch("galaxy.rest.planet_routes.get_planet_list")
     def test_get_planet_list_route(self, mock_builder):
-        mock_builder.return_value = jsonify({})
+        """
+            Unit: Route: Planet: Test method in route /planets/
+        """
+        mock_builder.return_value = jsonify(self.list_planet)
+        planets_expec = get_planet_list
 
+        self.assertEqual(planets_expec, self.list_planet)
